106a843f2bb043454939ba5bfb4636f7b7e28370
Marenqing <1213642578@qq.com>
2014-11-05 09:27:57 +0800

    shishi

---
+++ backInfo/backInfo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  backInfo
+//
+//  Created by apple on 14-11-5.
+//  Copyright (c) 2014年 marenqing. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
